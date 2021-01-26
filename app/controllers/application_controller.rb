6027bc3dcafbed4aad56de78f85ba4b949c356d0
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-01-27 01:00:33 +0900

    20210126_testdata_in_bookers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up,keys:[:email,:name])
       devise_parameter_sanitizer.permit(:sign_in,keys:[:name])
     end
+
+    private
+
 end
