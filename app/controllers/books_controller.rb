8b4e8914dc36536eb7a6d8f947208aaaadd717e1
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-01-31 11:02:24 +0900

    20210131_bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
     def show
         @book = Book.new
         @books = Book.find(params[:id])
-        @user = current_user
+        @user = User.find(@books.user_id)
     end
 
     def create
@@ -46,7 +46,7 @@ class BooksController < ApplicationController
 
     private
     def book_params
-        params.require(:book).permit(:title, :body)
+        params.require(:book).permit(:title, :body,)
     end
 
 end
