698be21d683da5cbbe7daaec1dd634297a3240c1
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-02-12 06:17:54 +0900

    20210212_bookers_fix_end

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
     end
 
     def edit
-        @book = Book.find(params[:id])
+        @ book = Book.find(params[:id])
         if @book.user_id != current_user.id
             redirect_to books_path
         end
