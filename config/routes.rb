f0656f1ab78a393089a68bfede1f28f7c84f420a
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-02-15 21:26:52 +0900

    20210215_bookers_iine_add_1/2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :index, :edit, :update]
-  resources :books, only: [:show, :index, :create, :destroy, :edit, :update]do
+  resources :books, only: [:show, :index, :create, :destroy, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   get 'home/about' => 'homes#about' , as: 'about'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_14_051917) do
+ActiveRecord::Schema.define(version: 2021_02_14_134412) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_02_14_051917) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
