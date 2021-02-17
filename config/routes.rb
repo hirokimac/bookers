2a6e1f4a5c6f28a4e6e666c8600a088d76d224d1
hirokimac <hiroki.yokomizo1030@gmail.com>
2021-02-17 21:46:53 +0900

    20210217_bookers_follow_follower_add_middle

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   end
   get 'home/about' => 'homes#about' , as: 'about'
   root 'homes#top'
+  post 'follow/:id' => 'relationships#follow' , as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow' , as: 'unfollow'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
